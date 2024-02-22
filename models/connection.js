62b69974d59e9124ae5add780f9daa2dc76cc75f
NicolasNgu23 <nicolasnguyen0023@hotmail.com>
2024-02-22 15:32:30 +0100

    final push

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://nicolasnguyen0023:iffy@cluster0.zat8bpq.mongodb.net/weatherapp';
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
